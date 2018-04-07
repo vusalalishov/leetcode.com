a005b99aea6223f0d0d154f51e8b4be2e7d82949
Vusal Alishov <vusal.alishov@gmail.com>
2018-04-07 20:36:00 +0400

    Solution for problem 9 - Palindrome Number - Added faster solution

--- src/easy/p9/Solution.java
+++ src/easy/p9/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.isPalindrome2(11));
+        System.out.println(solution.isPalindrome3(121));
     }
 
     // 3%
@@ -79,6 +79,24 @@ public class Solution {
 
         return top == 0;
 
+    }
+
+    // 88%
+    public boolean isPalindrome3(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+        if (x < 10) {
+            return true;
+        }
+
+        int tmp = 0;
+        while (x > tmp) {
+            tmp = tmp * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == tmp || x == tmp / 10;
     }
 
 }
