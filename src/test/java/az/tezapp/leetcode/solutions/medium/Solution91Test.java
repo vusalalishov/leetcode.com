3389fc17bd1a34fefc2934b7d3a32a10d90e2e57
Vusal Alishov <vusal.alishov@gmail.com>
2018-05-31 10:44:15 +0400

    Junit jupiter - for testing

---
+++ src/test/java/az/tezapp/leetcode/solutions/medium/Solution91Test.java
@@ -0,0 +1,26 @@
+package az.tezapp.leetcode.solutions.medium;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution91Test {
+
+    private Solution91 solution = new Solution91();
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "24726, 4",
+            "10, 1",
+            "1, 1",
+            "12, 2",
+            "110, 1",
+            "101, 1",
+            "200, 0"
+    })
+    void testNumDecodings(String s, int num) {
+        assertEquals(solution.numDecodings(s), num);
+    }
+
+}
