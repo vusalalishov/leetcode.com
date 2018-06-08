5a01605a6a91ac3189e71a9acc210106134e771b
Vusal Alishov <vusal.alishov@gmail.com>
2018-06-08 10:55:31 +0400

    Problem [22] - Generate Parentheses

---
+++ src/main/java/az/tezapp/leetcode/solutions/medium/Solution22.java
@@ -0,0 +1,60 @@
+package az.tezapp.leetcode.solutions.medium;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution22 {
+
+    // TODO: to be optimized
+    // ACCPETED - 7%
+    public List<String> generateParenthesis(int n) {
+        if (n == 0) {
+            return Collections.emptyList();
+        }
+        List<String> out = new ArrayList<>();
+        List<String> comb = comb(n * 2, 1, 1, 0);
+        for (int i = 0; i < comb.size(); i++) {
+            out.add("(" + comb.get(i));
+        }
+        return out;
+    }
+
+    private List<String> comb(int n, int i, int openCount, int closeCount) {
+
+        if (i == n - 1) {
+            return Collections.singletonList(")");
+        }
+
+        int tillEnd = n - i - 1;
+        List<String> out = new ArrayList<>();
+        if (openCount >= closeCount) {
+            int diff = openCount - closeCount;
+            if ((openCount > closeCount && diff < tillEnd) || openCount == closeCount) {
+                List<String> comb2 = comb(n, i + 1, openCount + 1, closeCount);
+                if (comb2.isEmpty()) {
+                    out.add("(");
+                } else {
+                    for (String s : comb2) {
+                        out.add("(" + s);
+                    }
+                }
+            }
+
+            if (openCount > closeCount) {
+                List<String> comb1 = comb(n, i + 1, openCount, closeCount + 1);
+                if (comb1.isEmpty()) {
+                    out.add(")");
+                } else {
+                    for (String s : comb1) {
+                        out.add(")" + s);
+                    }
+                }
+            }
+
+        }
+
+        return out;
+    }
+
+}
---
+++ src/test/java/az/tezapp/leetcode/solutions/medium/Solution22Test.java
@@ -0,0 +1,17 @@
+package az.tezapp.leetcode.solutions.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Solution22Test {
+
+    private Solution22 subject = new Solution22();
+
+    @Test
+    void testGenerateParenthesis() {
+        List<String> strings = subject.generateParenthesis(1);
+    }
+
+
+}
