a8fc8c6dfc6099daa665f9df819c7d7e14d028aa
Vusal Alishov <vusal.alishov@gmail.com>
2018-06-12 11:45:24 +0400

    Problem [28] - Implement strStr()

---
+++ src/test/java/az/tezapp/leetcode/solutions/easy/Solution28Test.java
@@ -0,0 +1,22 @@
+package az.tezapp.leetcode.solutions.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class Solution28Test {
+
+    private Solution28 subject = new Solution28();
+
+    @Test
+    void testStrStr() {
+        assertThat(subject.strStr("hello", "ll")).isEqualTo(2);
+        assertThat(subject.strStr("hello", "l")).isEqualTo(2);
+        assertThat(subject.strStr("hello", "he")).isEqualTo(0);
+        assertThat(subject.strStr("hello", "e")).isEqualTo(1);
+        assertThat(subject.strStr("hello", "h")).isEqualTo(0);
+        assertThat(subject.strStr("hello", "")).isEqualTo(0);
+        assertThat(subject.strStr("hello", "as")).isEqualTo(-1);
+    }
+
+}
