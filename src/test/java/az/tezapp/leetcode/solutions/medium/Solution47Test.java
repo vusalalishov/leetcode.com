ea8c8cbb027fa7b3c504b476eb7da308b987650a
Vusal Alishov <vusal.alishov@gmail.com>
2018-06-19 11:38:44 +0400

    Problem [47] - Permutations II[to be optimized]

---
+++ src/test/java/az/tezapp/leetcode/solutions/medium/Solution47Test.java
@@ -0,0 +1,16 @@
+package az.tezapp.leetcode.solutions.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class Solution47Test {
+
+    private Solution47 subject = new Solution47();
+
+    @Test
+    void testPermute() {
+        List<List<Integer>> permute = subject.permuteUnique(new int[]{1, 1, 2});
+    }
+
+}
