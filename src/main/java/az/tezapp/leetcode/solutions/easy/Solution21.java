7b3b3dd2da46e54260f7b66b754ebcc93b0d7dd7
Vusal Alishov <vusal.alishov@gmail.com>
2018-06-21 07:50:03 +0400

    Problem [21] - Merge Two Sorted Lists

---
+++ src/main/java/az/tezapp/leetcode/solutions/easy/Solution21.java
@@ -0,0 +1,33 @@
+package az.tezapp.leetcode.solutions.easy;
+
+public class Solution21 {
+
+    // 95% - ACCEPTED
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        ListNode root = l1.val < l2.val ? l1 : l2;
+        ListNode mergePointer = l1 == root ? l2 : l1;
+        while (mergePointer != null) {
+            ListNode rootPointer = root;
+            while (rootPointer != null) {
+                if (rootPointer.next == null) {
+                    rootPointer.next = new ListNode(mergePointer.val);
+                    break;
+                }
+                if (rootPointer.next.val > mergePointer.val) {
+                    ListNode toInsert = new ListNode(mergePointer.val);
+                    toInsert.next = rootPointer.next;
+                    rootPointer.next = toInsert;
+                    break;
+                } else {
+                    rootPointer = rootPointer.next;
+                }
+            }
+            mergePointer = mergePointer.next;
+        }
+        return root;
+    }
+
+}
---
+++ src/test/java/az/tezapp/leetcode/solutions/easy/Solution21Test.java
@@ -0,0 +1,26 @@
+package az.tezapp.leetcode.solutions.easy;
+
+import org.junit.jupiter.api.Test;
+
+class Solution21Test {
+
+    private Solution21 subject = new Solution21();
+
+    @Test
+    void testMergeTwoLists() {
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode listNode = subject.mergeTwoLists(l1, l2);
+
+        System.out.println(listNode);
+
+    }
+
+}
