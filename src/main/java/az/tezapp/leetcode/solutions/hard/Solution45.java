6d3ad00ad419c499b2061949945773486ea23ba6
Vusal Alishov <vusal.alishov@gmail.com>
2018-06-29 19:11:32 +0400

    Problem [45] - Jump Game II

---
+++ src/main/java/az/tezapp/leetcode/solutions/hard/Solution45.java
@@ -0,0 +1,34 @@
+package az.tezapp.leetcode.solutions.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution45 {
+
+    // ACCEPTED - 3% - TODO: optimize it
+    public int jump(int[] nums) {
+        int len = nums.length;
+
+        if (len < 2) {
+            return 0;
+        }
+
+        List<Integer> jumps = new ArrayList<>();
+        boolean jumped = false;
+        int jumpTo = len - 1;
+        while (!jumped) {
+            for (int i = 0; i < jumpTo; i++) {
+                if (nums[i] >= jumpTo - i) {
+                    jumps.add(i);
+                    jumpTo = i;
+                    break;
+                }
+            }
+            if (jumpTo == 0) {
+                jumped = true;
+            }
+        }
+        return jumps.size();
+    }
+
+}
