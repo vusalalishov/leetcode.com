1c60e8d8c22b866fb2f2a5fff3edae5957c3bcdc
Vusal Alishov <vusal.alishov@nitka.com>
2018-07-10 11:44:28 +0400

    Problem [35] - Search Insert Position

---
+++ src/test/java/az/tezapp/leetcode/solutions/easy/Solution35Test.java
@@ -0,0 +1,19 @@
+package az.tezapp.leetcode.solutions.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Solution35Test {
+
+    private Solution35 subject = new Solution35();
+
+    @Test
+    void testSearchInsert() {
+        assertThat(subject.searchInsert(new int[]{-20, 1, 2, 3}, 1)).isEqualTo(1);
+        assertThat(subject.searchInsert(new int[]{-9, -5, -1, 1, 2, 3}, 10)).isEqualTo(6);
+        assertThat(subject.searchInsert(new int[]{-9, -5, -1, 1, 2, 3}, -20)).isEqualTo(0);
+        assertThat(subject.searchInsert(new int[]{1, 3, 5, 6}, 2)).isEqualTo(1);
+    }
+
+}
