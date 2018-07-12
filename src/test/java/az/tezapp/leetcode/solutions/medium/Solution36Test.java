46fbdd56e98a8828b40f176dad99e890a426c819
Vusal Alishov <vusal.alishov@nitka.com>
2018-07-12 13:26:52 +0400

    Problem [36] - Valid Sudoku

---
+++ src/test/java/az/tezapp/leetcode/solutions/medium/Solution36Test.java
@@ -0,0 +1,27 @@
+package az.tezapp.leetcode.solutions.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Solution36Test {
+
+    private Solution36 subject = new Solution36();
+
+    @Test
+    void testIsValidSudoku() {
+        char[][] source = new char[][] {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+        assertThat(subject.isValidSudoku(source)).isTrue();
+    }
+
+}
