3caf4189ab60bc1a3f8e337e7efdfcb99e90bd5f
Vusal Alishov <vusal.alishov@nitka.com>
2018-08-01 14:34:10 +0400

    Problem [55] - Jump Game

---
+++ src/main/java/az/tezapp/leetcode/solutions/medium/Solution55.java
@@ -0,0 +1,42 @@
+package az.tezapp.leetcode.solutions.medium;
+
+public class Solution55 {
+
+    // ACCEPTED - 79%
+    public boolean canJump(int[] nums) {
+
+        int len = nums.length;
+        if (len <= 1) {
+            return true;
+        }
+
+        int pos = len - 2;
+        boolean broken = false;
+        while (pos >= 0 && !broken) {
+            if (nums[pos] == 0) {
+                if (pos - 1 < 0) {
+                    broken = true;
+                    break;
+                }
+
+                boolean jumped = false;
+                int jumpsRequired = 2;
+
+                for (int i = pos - 1; i >= 0; i--) {
+                    if (nums[i] >= jumpsRequired) {
+                        jumped = true;
+                        pos = i;
+                        break;
+                    }
+                    jumpsRequired++;
+                }
+                broken = !jumped;
+            } else {
+                pos--;
+            }
+        }
+
+        return !broken;
+    }
+
+}
---
+++ src/test/java/az/tezapp/leetcode/solutions/medium/Solution55Test.java
@@ -0,0 +1,22 @@
+package az.tezapp.leetcode.solutions.medium;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class Solution55Test {
+
+    private Solution55 subject = new Solution55();
+
+    @Test
+    void testCanJump() {
+        assertThat(subject.canJump(new int[] {0})).isTrue();
+        assertThat(subject.canJump(new int[] {1})).isTrue();
+        assertThat(subject.canJump(new int[] {0})).isTrue();
+        assertThat(subject.canJump(new int[] {2,3,1,1,4})).isTrue();
+        assertThat(subject.canJump(new int[] {3,2,1,0,4})).isFalse();
+        assertThat(subject.canJump(new int[] {0,1,1,1,1,1})).isFalse();
+        assertThat(subject.canJump(new int[] {0,1,1,0,1,1})).isFalse();
+        assertThat(subject.canJump(new int[] {4,1,1,0,1,1})).isTrue();
+    }
+
+}
