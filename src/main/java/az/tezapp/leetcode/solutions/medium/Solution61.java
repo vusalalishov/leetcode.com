22c0f9eb7cb2be51969195919f39778930d97666
Vusal Alishov <vusal.alishov@nitka.com>
2018-08-04 08:34:25 +0400

    Problem [61] - Rotate List [Attempt to optimize]

--- src/main/java/az/tezapp/leetcode/solutions/medium/Solution61.java
+++ src/main/java/az/tezapp/leetcode/solutions/medium/Solution61.java
@@ -1,65 +1,56 @@
 package az.tezapp.leetcode.solutions.medium;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution61 {
 
-    // ACCEPTED - 18% [TODO: optimize]
+    // ACCEPTED - 19% [TODO: optimize]
     public ListNode rotateRight(ListNode head, int k) {
         if (k == 0 || head == null) {
             return head;
         }
 
-        int len = findLength(head);
-
-        k = k % len;
-
-        int valueHolder = head.val;
-        ListNode mainPointer = head;
-        ListNode pointer = mainPointer;
-
-        int moved = 0;
-        while (moved < len) {
-            int c = k;
-            while (c > 0) {
-                c--;
-                pointer = pointer.next != null ? pointer.next : head;
-            }
-            int tmpVal = pointer.val;
-            pointer.val = valueHolder;
-            valueHolder = tmpVal;
-            moved++;
-            if (pointer == mainPointer) {
-                mainPointer = mainPointer.next;
-                pointer = mainPointer;
-                if (pointer != null) {
-                    valueHolder = pointer.val;
-                }
-            }
+        List<Integer> source = new ArrayList<>();
+        ListNode pointer = head;
+        int len = 0;
+        while (pointer != null) {
+            source.add(pointer.val);
+            pointer = pointer.next;
+            len++;
         }
+        List<Integer> target = new ArrayList<>(source);
+        int jump = k % len;
+        for (int i = 0; i < len; i++) {
+            target.set((i + jump) % len, source.get(i));
+        }
+
+        ListNode output = head;
+        output.val = target.get(0);
+        output = output.next;
+        int i = 1;
+        while (output != null) {
+            output.val = target.get(i);
+            output = output.next;
+            i++;
+        }
+
         return head;
-    }
-
-    private int findLength(ListNode node) {
-        int c = 0;
-        while (node != null) {
-            c++;
-            node = node.next;
-        }
-        return c;
     }
 
     public static void main(String[] args) {
         Solution61 sol = new Solution61();
         ListNode l1 = new ListNode(1);
-//        l1.next = new ListNode(2);
-//        l1.next.next = new ListNode(3);
-//        l1.next.next.next = new ListNode(4);
-//        l1.next.next.next.next = new ListNode(5);
-//        l1.next.next.next.next.next = new ListNode(6);
-//        l1.next.next.next.next.next.next = new ListNode(7);
-//        l1.next.next.next.next.next.next.next = new ListNode(8);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+        l1.next.next.next.next.next = new ListNode(6);
+        l1.next.next.next.next.next.next = new ListNode(7);
+        l1.next.next.next.next.next.next.next = new ListNode(8);
         System.out.println(l1);
 //        System.out.println(sol.rotateRight(l1, 14));
-        System.out.println(sol.rotateRight(l1, 1));
+        System.out.println(sol.rotateRight(l1, 2000001));
     }
 
 }
