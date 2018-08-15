243b83c5be7807e1aa7260e478ed84595dbacab2
Vusal Alishov <vusal.alishov@nitka.com>
2018-08-15 10:12:00 +0400

    Problem [66] - Plus One

---
+++ src/test/java/az/tezapp/leetcode/solutions/easy/Solution66Test.java
@@ -0,0 +1,17 @@
+package az.tezapp.leetcode.solutions.easy;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class Solution66Test {
+
+    private Solution66 subject = new Solution66();
+
+    @Test
+    void testPlusOne() {
+        assertThat(subject.plusOne(new int[] {1, 2, 3})).containsExactly(1, 2, 4);
+        assertThat(subject.plusOne(new int[] {1, 2, 9})).containsExactly(1, 3, 0);
+        assertThat(subject.plusOne(new int[] {9, 9, 9})).containsExactly(1, 0, 0, 0);
+    }
+
+}
