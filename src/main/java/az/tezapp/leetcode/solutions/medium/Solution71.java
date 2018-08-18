9768e73872620166ba8b7a226a2822a85a0c9f01
Vusal Alishov <vusal.alishov@nitka.com>
2018-08-18 16:27:04 +0400

    Problem [71] - Simplify Path

---
+++ src/main/java/az/tezapp/leetcode/solutions/medium/Solution71.java
@@ -0,0 +1,39 @@
+package az.tezapp.leetcode.solutions.medium;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution71 {
+
+    // ACCEPTED: 26%
+    public String simplifyPath(String path) {
+
+        String[] source = path.split("/");
+        Deque<String> deque = new LinkedList<>();
+
+        for (String tmp : source) {
+            if (tmp.isEmpty() || tmp.equals(".")) {
+                continue;
+            }
+            if (tmp.equals("..")) {
+                if (!deque.isEmpty()) {
+                    deque.removeFirst();
+                }
+                continue;
+            }
+            deque.addFirst(tmp);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            sb.append("/").append(deque.removeLast());
+        }
+
+        if (sb.length() == 0) {
+            sb.append("/");
+        }
+
+        return sb.toString();
+    }
+
+}
