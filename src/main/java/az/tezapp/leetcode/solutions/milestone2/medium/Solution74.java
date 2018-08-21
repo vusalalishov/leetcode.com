5524f0b72a2f9506de412957050fff58eeabb984
Vusal Alishov <vusal.alishov@nitka.com>
2018-08-21 12:59:32 +0400

    Problem [74] - Search a 2D Matrix[Optimized]

--- src/main/java/az/tezapp/leetcode/solutions/milestone2/medium/Solution74.java
+++ src/main/java/az/tezapp/leetcode/solutions/milestone2/medium/Solution74.java
@@ -2,8 +2,72 @@ package az.tezapp.leetcode.solutions.milestone2.medium;
 
 public class Solution74 {
 
-    // ACCEPTED - 7% [todo: optimize]
-    public boolean searchMatrix(int[][] matrix, int target) {
+    // ACCEPTED - 92%
+    @SuppressWarnings("Duplicates")
+    public boolean searchMatrixOptimized2(int[][] matrix, int target) {
+        int rowCount = matrix.length;
+        if (rowCount == 0) {
+            return false;
+        }
+        int colCount = matrix[0].length;
+        if (colCount == 0) {
+            return false;
+        }
+        int from = 0;
+        int to = rowCount * colCount - 1;
+        while (to - from > 1) {
+            int middle = (from + to) / 2;
+            int row = middle / colCount;
+            int col = middle % colCount;
+            if (target <= matrix[row][col]) {
+                to = middle;
+            } else {
+                from = middle;
+            }
+        }
+
+        return isEqual(matrix, target, from, colCount) || isEqual(matrix, target, to, colCount);
+    }
+
+    private boolean isEqual(int[][] matrix, int target, int index, int colCount) {
+        int row = index / colCount;
+        int col = index % colCount;
+        return matrix[row][col] == target;
+    }
+
+    // ACCEPTED - 3%
+    @SuppressWarnings("Duplicates")
+    public boolean searchMatrixOptimized(int[][] matrix, int target) {
+        int rowCount = matrix.length;
+        if (rowCount == 0) {
+            return false;
+        }
+        int colCount = matrix[0].length;
+        if (colCount == 0) {
+            return false;
+        }
+        int len = rowCount * colCount;
+        return binarySearchMatrix(matrix, target, 0, len - 1, colCount);
+    }
+
+    private boolean binarySearchMatrix(int[][] matrix, int target, int from, int to, int colCount) {
+        if (from == to) {
+            int row = from / colCount;
+            int col = from % colCount;
+            return matrix[row][col] == target;
+        }
+        int middle = (from + to) / 2;
+        int row = middle / colCount;
+        int col = middle % colCount;
+        if (target <= matrix[row][col]) {
+            return binarySearchMatrix(matrix, target, from, middle, colCount);
+        } else {
+            return binarySearchMatrix(matrix, target, middle + 1, to, colCount);
+        }
+    }
+
+    // ACCEPTED - 7%
+    public boolean searchMatrixOriginal(int[][] matrix, int target) {
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
--- src/test/java/az/tezapp/leetcode/solutions/milestone2/medium/Solution74Test.java
+++ src/test/java/az/tezapp/leetcode/solutions/milestone2/medium/Solution74Test.java
@@ -9,29 +9,87 @@ class Solution74Test {
 
     @Test
     void testSearchMatrix() {
-        assertThat(subject.searchMatrix(new int[][] {
+        assertThat(subject.searchMatrixOriginal(new int[][] {
                 {1,   3,  5,  7},
                 {10, 11, 16, 20},
                 {23, 30, 34, 50}
         }, 3 )).isTrue();
 
-        assertThat(subject.searchMatrix(new int[][] {
+        assertThat(subject.searchMatrixOriginal(new int[][] {
                 {1,   3,  5,  7},
                 {10, 11, 16, 20},
                 {23, 30, 34, 50}
         }, 44 )).isFalse();
 
-        assertThat(subject.searchMatrix(new int[][] {
+        assertThat(subject.searchMatrixOriginal(new int[][] {
                 {1,   3,  5,  7},
                 {10, 11, 16, 20},
                 {23, 30, 34, 50}
         }, 50 )).isTrue();
 
-        assertThat(subject.searchMatrix(new int[][] {
+        assertThat(subject.searchMatrixOriginal(new int[][] {
                 {1}
         }, 2 )).isFalse();
 
-        assertThat(subject.searchMatrix(new int[][] {
+        assertThat(subject.searchMatrixOriginal(new int[][] {
+                {1}
+        }, 0 )).isFalse();
+    }
+
+    @Test
+    void testSearchMatrixOptimized() {
+        assertThat(subject.searchMatrixOptimized(new int[][] {
+                {1,   3,  5,  7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50}
+        }, 3 )).isTrue();
+
+        assertThat(subject.searchMatrixOptimized(new int[][] {
+                {1,   3,  5,  7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50}
+        }, 44 )).isFalse();
+
+        assertThat(subject.searchMatrixOptimized(new int[][] {
+                {1,   3,  5,  7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50}
+        }, 50 )).isTrue();
+
+        assertThat(subject.searchMatrixOptimized(new int[][] {
+                {1}
+        }, 2 )).isFalse();
+
+        assertThat(subject.searchMatrixOptimized(new int[][] {
+                {1}
+        }, 0 )).isFalse();
+    }
+
+    @Test
+    void testSearchMatrixOptimized2() {
+        assertThat(subject.searchMatrixOptimized2(new int[][] {
+                {1,   3,  5,  7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50}
+        }, 3 )).isTrue();
+
+        assertThat(subject.searchMatrixOptimized2(new int[][] {
+                {1,   3,  5,  7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50}
+        }, 44 )).isFalse();
+
+        assertThat(subject.searchMatrixOptimized2(new int[][] {
+                {1,   3,  5,  7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 50}
+        }, 50 )).isTrue();
+
+        assertThat(subject.searchMatrixOptimized2(new int[][] {
+                {1}
+        }, 2 )).isFalse();
+
+        assertThat(subject.searchMatrixOptimized2(new int[][] {
                 {1}
         }, 0 )).isFalse();
     }
