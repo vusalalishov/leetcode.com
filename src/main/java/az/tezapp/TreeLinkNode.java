f583ad34f9db889ac5cced9279bd03b612ebe30a
vusal.alishov <vusal.alishov@gmail.com>
2019-01-10 12:21:29 +0400

    Problem [116] - Populating Next Right Pointers in Each Node[Implemented solution from discussion tab]

---
+++ src/main/java/az/tezapp/TreeLinkNode.java
@@ -0,0 +1,7 @@
+package az.tezapp;
+
+public class TreeLinkNode {
+    public int val;
+    public TreeLinkNode left, right, next;
+    public TreeLinkNode(int x) { val = x; }
+}
--- src/main/java/az/tezapp/leetcode/solutions/milestone3/Solution116.java
+++ src/main/java/az/tezapp/leetcode/solutions/milestone3/Solution116.java
@@ -22,4 +22,19 @@ public class Solution116 {
         connect(left.right, right.left);
     }
 
+    // After checking discussion
+    public void connectDiscussed(TreeLinkNode root) {
+        if (root == null) {
+            return;
+        }
+        TreeLinkNode left = root.left, right = root.right;
+        connect(left);
+        connect(right);
+        while (left != null && right != null) {
+            left.next = right;
+            left = left.right;
+            right = right.left;
+        }
+    }
+
 }
