4ac745344a56e19501ee26eefc5c392f07b6b123
vusal.alishov <vusal.alishov@gmail.com>
2019-02-06 10:09:20 +0400

    Problem [133] - Clone Graph [optimized - beats 100%]

--- src/main/java/az/tezapp/leetcode/solutions/milestone3/medium/Solution133.java
+++ src/main/java/az/tezapp/leetcode/solutions/milestone3/medium/Solution133.java
@@ -1,31 +1,27 @@
 package az.tezapp.leetcode.solutions.milestone3.medium;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import az.tezapp.leetcode.solutions.UndirectedGraphNode;
 
 public class Solution133 {
 
-    // ACCEPTED - 78%
+    // ACCEPTED - 100% - optimized
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if (node == null) {
             return null;
         }
-        return cloneGraph(node, new HashSet<>(), new HashMap<>());
+        return cloneGraph(node, new HashMap<>());
     }
 
     private UndirectedGraphNode cloneGraph(UndirectedGraphNode node,
-                                           Set<Integer> visited,
                                            Map<Integer, UndirectedGraphNode> labelToNodeMap) {
         UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
-        visited.add(node.label);
         labelToNodeMap.put(node.label, newNode);
         for (UndirectedGraphNode neighbor : node.neighbors) {
-            if (!visited.contains(neighbor.label)) {
-                newNode.neighbors.add(cloneGraph(neighbor, visited, labelToNodeMap));
+            if (!labelToNodeMap.containsKey(neighbor.label)) {
+                newNode.neighbors.add(cloneGraph(neighbor, labelToNodeMap));
             } else {
                 newNode.neighbors.add(labelToNodeMap.get(neighbor.label));
             }
